d6e2bc50127e0b53954d239233f8a0410e3320c4
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-05 14:22:41 +0100

    commit

---
+++ 30-Days-Of-Python-master/03_Day_Operators/exercise3.py
@@ -0,0 +1,20 @@
+
+## Exercise 3
+# 1. Declare your age as integer variable
+# 2. Declare your height as a float variable
+# 3. Declare a variable that store a complex number
+# 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
+# ```py
+#     Enter base: 20
+#     Enter height: 10
+#     The area of the triangle is 100
+# ```
+# age = 34
+# height = 10
+# myNum = ai + 8j
+
+base = float(input('Enter the base of the triangle: '))
+height = float(input('Enter the height of the triangle:'))
+area = 0.5 * height * base
+print('The area of the triangle is: ', area)
